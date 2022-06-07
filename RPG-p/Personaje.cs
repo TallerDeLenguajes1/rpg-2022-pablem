6e84538404f27876acfee2d71b3e996f1a9c2a9a
pablo <palmierijpablo@gmail.com>
2022-06-06 22:12:21 -0300

    avances

--- RPG-p/Personaje.cs
+++ RPG-p/Personaje.cs
@@ -1,25 +1,74 @@
 public class Personaje
 {
-    // Tipos tipo;
-    // string nombre;
-    // string apodo;
-    // DateTime fechaNacimiento;
-    // //int Edad; //entre 0 y 300
-    // int Salud; //100%
-    // int velocidad; // 1 a 10
-    // int destreza; // 1 a 5
-    // int fuerza; //1 a 10
-    // int nivel; //1 a 10
-    // int armadura; //1 a 10
+    /* DATOS */
+    Tipos tipo;
+    string nombre;
+    string apodo;
+    DateTime fechaNacimiento;
+    int salud; //100%
 
-    Datos datos = new Datos();
-    Caracteristicas caracteristicas = new Caracteristicas();
+    /* CARACTERÍSTICAS */
+    int velocidad; // 1 a 10
+    int destreza; // 1 a 5
+    int fuerza; //1 a 10
+    int nivel; //1 a 10
+    int armadura; //1 a 10
 
-    public Datos Datos { get => datos; set => datos = value; }
-    public Caracteristicas Caracteristicas { get => caracteristicas; set => caracteristicas = value; }
+    public Tipos Tipo { get => tipo; set => tipo = value; }
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Apodo { get => apodo; set => apodo = value; }
+    public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+    public int Salud { get => salud; set => salud = value; }
+    public int Velocidad { get => velocidad; set => velocidad = value; }
+    public int Destreza { get => destreza; set => destreza = value; }
+    public int Fuerza { get => fuerza; set => fuerza = value; }
+    public int Nivel { get => nivel; set => nivel = value; }
+    public int Armadura { get => armadura; set => armadura = value; }
 
-    public void cargarDatos()
+    public void generarDatos() //No aleatorio?
     {
-        
+        var rnd = new Random();
+        Tipo = (Tipos)rnd.Next(0,8);
+        // Edad = rnd.Next(0,109572); // Edad max = 300*365.2425 = 109572
+        FechaNacimiento = DateTime.Today.AddDays(rnd.Next(-109572,0));
+        Salud = 100;
+    }
+    public void generarCaracteristicas() 
+    {
+        var rnd = new Random();
+        Velocidad = rnd.Next(1,10); //distribucón??
+        Destreza = rnd.Next(1,5);
+        Fuerza = rnd.Next(1,10);
+        Nivel = 1;
+        Armadura = rnd.Next(1,10);
+    }
+    public int calcularEdad()
+    {
+        return Convert.ToInt32((DateTime.Today - FechaNacimiento).Days/365.2425);
+    }
+    public int poderDisparo()
+    {
+        return Destreza*Fuerza*Nivel;
+    }
+    public int poderDefensa()
+    {
+        return Armadura*Velocidad;
+    }
+    public void mostrarDatos()
+    {
+        Console.WriteLine("\nDatos de "+Nombre+" (aka "+Apodo+")");
+        Console.WriteLine("\tClase: "+Tipo);
+        Console.WriteLine("\tFecha de Nacimiento: "+FechaNacimiento.ToString("dd/MM/yyyy"));
+        Console.WriteLine("\tEdad: "+calcularEdad()+" años");
+        Console.WriteLine("\tSalud: "+Salud+"%");
+    }
+    public void mostrarCaracteristicas()
+    {
+        Console.WriteLine("\nCaracterísticas de "+Nombre+" (aka "+Apodo+")");
+        Console.WriteLine("\tNivel "+Nivel);
+        Console.WriteLine("\tVelocidad: "+Velocidad);
+        Console.WriteLine("\tDestreza: "+Destreza);
+        Console.WriteLine("\tFuerza: "+Fuerza);
+        Console.WriteLine("\tArmadura: "+Armadura);
     }
 }
--- RPG-p/Program.cs
+++ RPG-p/Program.cs
@@ -3,7 +3,14 @@ var personajesENJuego = new List<Personaje>();
 Personaje nuevo; 
 for (int i = 0; i < 4; i++) {
     nuevo = new Personaje();
-///
-//
+    nuevo.Nombre = "Persona-"+i;
+    nuevo.Apodo = "Apodo-"+i;
+    nuevo.generarDatos();
+    nuevo.generarCaracteristicas();
     personajesENJuego.Add(nuevo);
+}
+foreach (var persona in personajesENJuego)
+{
+    persona.mostrarDatos();
+    persona.mostrarCaracteristicas();
 }
